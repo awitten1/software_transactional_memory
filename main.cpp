7d218515295eeb581594663297c8d7cc81b01b89
Andrew Witten <awitten007@gmail.com>
2026-09-19 22:50:22 -0400

    Add selectable STM backends

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 
 
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <thread>
 #include <cassert>
+
+#ifdef STM_USE_USTM
+#include <ustm.h>
+#endif
 
 int balance_a = 100'000;
 int balance_b = 100'000;
@@ -13,6 +18,17 @@ void transfer() {
         int amount = rand() % 10'000;
         bool a_to_b = rand() & 1;
 
+#ifdef STM_USE_USTM
+        ustm::transaction([&] {
+            int from = ustm::load(a_to_b ? balance_a : balance_b);
+            int to = ustm::load(a_to_b ? balance_b : balance_a);
+            if (amount <= from) {
+                ustm::store(a_to_b ? balance_a : balance_b, from - amount);
+                ustm::store(a_to_b ? balance_b : balance_a, to + amount);
+            }
+            return true;
+        });
+#else
         __transaction_atomic {
             if (a_to_b) {
                 if (amount <= balance_a) {
@@ -26,15 +42,19 @@ void transfer() {
                 }
             }
         }
+#endif
     }
 }
 
 void reader() {
     for (int i = 0; i < 100'000; ++i) {
+#ifdef STM_USE_USTM
+        int t = ustm::transaction(
+            [&] { return ustm::load(balance_a) + ustm::load(balance_b); }, true);
+#else
         int t;
-        __transaction_atomic {
-            t = balance_a + balance_b;
-        }
+        __transaction_atomic { t = balance_a + balance_b; }
+#endif
         assert(t == 200'000);
     }
 }
